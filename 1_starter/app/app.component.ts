e86c24c5a46aff99ffad9e6ec2c2adcdfa833e00
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-07 19:40:59 -0500

    prepare files for data binding

--- 1_starter/app/app.component.ts
+++ 1_starter/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+/*
+separate features so we can: Isolate Test Reuse
+
+Building Components:
+Component:
+Template: View layout (HTML code, binding, directives)
+Class: code that supports the view, contains properties and methods
+Metadata: used inside @Decorator. Exta instruction for Angular. tells angular how to instantiate component, construct a view, how to interact with other components using directives.
+
+In angular, class becomes a component when it is given this metadata.
+*/
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>My Brand New Angular App</h1>'
+  moduleId: module.id, // needed if using systemJS as module loader
+  selector: 'bs-app', // bookstore app
+  templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+// <Feature><Type> = AppComponent
+export class AppComponent {
+  pageTitle: string = "Dev Book Store"
+}
