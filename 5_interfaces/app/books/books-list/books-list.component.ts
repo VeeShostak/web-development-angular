d14af87e916e39055d4a3ea0ea84d9ede1a8a478
VeeShostak <VeeShostak@users.noreply.github.com>
2018-01-09 00:49:40 -0500

    Implement interface to strongly type JSON data that would be returned from an api.

---
+++ 5_interfaces/app/books/books-list/books-list.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { IBook } from '../book';
+
+// interface IBook {
+//   bookAuthor: string;
+//   bookTitle: string;
+//   bookPrice: number;
+//   bookDescription: string;
+//   publishedOn: Date;
+//   inStock: string;
+//   bookReviews: number;
+//   bookImageUrl: string;
+//   hardcover?: boolean;
+// }
+
+@Component({
+  moduleId: module.id,
+  selector: 'bs-books-list',
+  templateUrl: 'books-list.component.html'
+})
+
+export class BooksListComponent {
+  imageWidth: number = 100;
+  showImage: boolean = true;
+  booksInStock: number = 2;
+
+  books: IBook[] = [{
+    bookAuthor: "Tom Jones",
+    bookTitle: "War and Peace 2",
+    bookPrice: 29.95,
+    bookDescription: "Book of historical fiction",
+    publishedOn: new Date('02/11/1921'),
+    inStock: "yes",
+    bookReviews: 15,
+    bookImageUrl: "app/assets/images/656.jpg"
+  }]
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+}
